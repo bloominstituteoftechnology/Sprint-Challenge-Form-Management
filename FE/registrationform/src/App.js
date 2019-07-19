d09cb87b2e36937a45f9b9059fefa8940690e153
Bryce-Soghigian <brybrycodingguy@gmail.com>
2019-07-19 10:16:17 -0500

    Set up router and APP for the rest of the project

--- FE/registrationform/src/App.js
+++ FE/registrationform/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Route, Link } from 'react-router-dom';
+import PrivateRoute from "./Components/SecretData/PrivateRoute"
 
+import Login from './Components/Login/Login'
+
+import LandingPage from './Components/LandingPage/LandingPage'
 function App() {
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    setToken(localStorage.getItem('token'))
+    console.log('test')
+  }, [token]);
+
+  console.log('Token u there brother?: ', token);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div >
+      <header >
+        <h1>React app</h1>
+        <Link  to='/'>
+          Home
+        </Link>
+        {token === null ? (
+          <Link to='/login'>
+            Login
+          </Link>
+        ) : (
+          <Link to='/login'>
+            <button onClick={() => {
+              localStorage.removeItem('token')
+              setToken()}} >
+              Logout
+            </button>
+          </Link>
+        )}
       </header>
+      <div>
+        <PrivateRoute exact path='/' component={LandingPage} />
+        <Route exact path='/login' render={(props) => <Login {...props} setToken={setToken} />} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
