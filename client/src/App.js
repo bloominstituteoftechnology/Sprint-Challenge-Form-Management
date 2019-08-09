ac4eada3dce70bdfbc8f2be19c6a048e4f0236f0
dangolbeeker <scottb305@outlook.com>
2019-08-09 13:37:33 -0400

    added form testing, navbar, react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import UserForm from './components/form';
+import React, { useState } from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/navbar';
+import Display from './components/display';
+import { FormikRegisterForm } from './components/form';
+import { FormikLoginForm } from './components/form';
 
 function App() {
+  const [token, setToken] = useState("");
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
-
-      <UserForm />
-
-    </div>
+    <Navbar />
+      <Route exact path='/' render={(props) => <FormikRegisterForm {...props} setToken={setToken} text="Register Form" />} />
+      <Route exact path='/login' render={(props) => <FormikLoginForm {...props} setToken={setToken} text="Login Form" />} />
+      <Route exact path='/display' render={(props) => <Display {...props} token={token} />} />
+      </div>
   );
 }
 
---
+++ client/src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar(props) {
+    return (
+        <div className="navbar">
+            <div className="navbar-right">
+                <Link to="/login" className="links">Login</Link>
+                <Link to="/" className="links">Register</Link>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
