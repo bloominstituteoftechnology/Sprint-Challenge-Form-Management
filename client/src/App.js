0e8017605f7c3f22936fd04cac08e7de77b86663
johnschneider1 <johnlawschneider@gmail.com>
2019-08-09 10:43:39 -0500

    questions and setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import SprintForm from "./components/SprintForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Sprint John!!!!!</h1>
+      <SprintForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/SprintForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Form, Field, withFormik } from "formik";
+import * as Yup from "yup";
+
+const SprintForm = () => {
+  return (
+    <div className="form-container">
+      <h1>Form Stuff!</h1>
+    </div>
+  );
+};
+
+export default SprintForm;
