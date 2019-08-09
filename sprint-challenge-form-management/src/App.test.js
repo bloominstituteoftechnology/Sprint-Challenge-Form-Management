a97cd34296c7be08b679feb6d03c225d9378de09
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-08-09 15:59:26 -0500

    trying to figure out testing

--- sprint-challenge-form-management/src/App.test.js
+++ sprint-challenge-form-management/src/App.test.js
@@ -1,6 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { render, fireEvent } from '@testing-library/react';
+import'@testing-library/react/cleanup-after-each';
+
+describe ('App.js file', () => {
+    it('App renders without crashing', () => {
+        render(<App />);
+    });
+})
 
 // it('renders without crashing', () => {
 //   const div = document.createElement('div');
