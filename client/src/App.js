b5b43867dfeb4b39655f4e9867a958f3e20dab54
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-08-09 11:15:33 -0700

    added tests for App and User

--- client/src/App.js
+++ client/src/App.js
@@ -4,54 +4,40 @@ import UserList from "./UserList";
 import axios from "axios";
 import "./App.css";
 
-// const users = [
-//   {
-//     username: "apatelthompson",
-//     id: "1",
-//     password: "123456",
-//     terms: true,
-//     location: "USA"
-//   }
-// ];
-
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       users: []
     };
+    console.log(this.state);
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  componentDidUpdate() {
+    console.log();
   }
 
   getData = response => {
     axios
       .get("http://localhost:5000/api/restricted/data")
       .then(response => {
-        console.log("response", response);
-        return response.json();
-      })
-      .then(user => {
-        console.log(user);
-        this.setState({ users: user });
+        console.log(response.data);
+        this.setState(response.data);
       })
       .catch(err => console.log("error"));
   };
 
-  addUser = user => {
-    const newUser = {
-      username: user.username,
-      id: Date.now(),
-      password: user.password
-    };
-    this.setState({ users: [...this.state.users, newUser] });
-  };
-
   render() {
-    console.log("data", this.state.data);
+    console.log("users", this.state.users);
     return (
       <div className="App">
         <h1>Welcome to our Site!</h1>
         <h3>Please sign up here:</h3>
-        <Form addUser={this.addUser} />
+        <Form />
         <UserList dataProp={this.state.users} />
       </div>
     );
--- client/src/User.test.js
+++ client/src/User.test.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import User from "./User";
 import { render } from "@testing-library/react";
 import "@testing-library/react/cleanup-after-each";
 import "jest-dom/extend-expect";
 
 afterEach(rtl.cleanup);
 
-describe("<App />", () => {
-  it("renders without crashing", () => {
-    render(<App />);
+describe("<User />", () => {
+  it("renders User without crashing", () => {
+    render(<User />);
   });
 });
